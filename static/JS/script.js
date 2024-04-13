d4532702acdd26ee82ac3d9ed07123b2af2799d6
Mohit Pandey <mp7698921@gmail.com>
2024-04-13 17:12:25 +0530

    ready to host

--- static/JS/script.js
+++ static/JS/script.js
@@ -59,14 +59,54 @@ const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
 
 link1.addEventListener('click', () => {
-  scrollToElement('.header');
+  scrollToElement('.header',1);
 });
 
 link2.addEventListener('click', () => {
   // Scroll to the second element with "header" class
-  scrollToElement('.header', 1);
+  scrollToElement('.header', 2);
 });
 
 link3.addEventListener('click', () => {
   scrollToElement('.column');
+});
+
+
+const link4 = document.getElementById("link4");
+const link5 = document.getElementById("link5");
+const link6 = document.getElementById("link6");
+
+link4.addEventListener('click', () => {
+  scrollToElement('.header', 1);
+});
+
+link5.addEventListener('click', () => {
+  // Scroll to the second element with "header" class
+  scrollToElement('.header', 2);
+});
+
+link6.addEventListener('click', () => {
+  scrollToElement('.column');
+});
+
+let fix = document.getElementById('fix');
+
+// Function to move the element to bottom -35 after 5 seconds
+function moveBottom() {
+    fix.style.bottom = "-40px";
+}
+
+// Set a timeout to call moveBottom after 5 seconds
+let timeoutId = setTimeout(moveBottom, 3000);
+
+// Function to reset the element to bottom 0 when the user clicks on the screen
+function resetBottom() {
+    fix.style.bottom = "0px";
+}
+
+// Event listener to reset the bottom position when the user clicks on the screen
+document.addEventListener('click', function(event) {
+    resetBottom();
+    clearTimeout(timeoutId); // Cancel the timeout to prevent it from triggering after a click
+    setTimeout(moveBottom, 3000);
 });
