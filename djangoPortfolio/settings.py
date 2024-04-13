dcc053259b05f29bc236aa140238b99cb908103c
Mohit Pandey <mp7698921@gmail.com>
2024-04-13 18:50:40 +0530

    host success

--- djangoPortfolio/settings.py
+++ djangoPortfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5nxl4ax3wl*t_8cb73%bxi4kk5w*j6=$_(#5++jt0vby(w*l0n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['mohitpandey.up.railway.app']
 # ALLOWED_HOSTS = ['*']
